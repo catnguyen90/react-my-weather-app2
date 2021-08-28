4a93a74a6893931b65d23f0b4d32ee75f88bbc80
catnguyen90 <catnguyen90@gmail.com>
2021-08-28 13:39:11 -0400

    Added GitHub Repo Url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Cathy Nguyen and is{" "}
+        <a
+          href="https://github.com/catnguyen90/react-my-weather-app2"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
