da687239b4c96433d28cdc5956879a94c08d4213
catnguyen90 <catnguyen90@gmail.com>
2021-08-28 16:24:29 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Cathy Nguyen and is{" "}
-        <a
-          href="https://github.com/catnguyen90/react-my-weather-app2"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Cathy Nguyen and is{" "}
+          <a
+            href="https://github.com/catnguyen90/react-my-weather-app2"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
