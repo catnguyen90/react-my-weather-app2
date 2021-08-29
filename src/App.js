456fd8fa22d40e8845c94647ab6ccd5d943372f5
catnguyen90 <catnguyen90@gmail.com>
2021-08-29 13:25:01 -0400

    added css to the weather component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
-        <footer>
-          This project was coded by Cathy Nguyen and is{" "}
-          <a
-            href="https://github.com/catnguyen90/react-my-weather-app2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            open-sourced on Github
-          </a>
-        </footer>
-      </div>
+      <Weather />
+      <footer>
+        This project was coded by Cathy Nguyen and is{" "}
+        <a
+          href="https://github.com/catnguyen90/react-my-weather-app2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -4,36 +4,43 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h4 className="text-center">Current Date and Time</h4>
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-            />
+      <div className="container">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
-      <h1 className="text-center">City</h1>
-      <h4 className="text-center">Description</h4>
-      <div className="row">
-        <div className="col-6 text-center">
-          <h4>Humidity: </h4>
-        </div>
-        <div className="col-6 text-center">
-          <h4>Wind: </h4>
-        </div>
-      </div>
+        </form>
 
-      <h2 className="text-center">
-        <img clasName="float-left" src="#" alt="Icon" />
-        Temp
-      </h2>
+        <div className="row">
+          <div className="col-6">
+            <h1 className="mt-3">City</h1>
+            <ul>
+              <li>Date and Time</li>
+              <li>Humidity: %</li>
+              <li>Wind: m/H</li>
+            </ul>
+          </div>
+          <div className="col-6">
+            <h2 className="float-left mt-3">Description</h2>
+            <h3>
+              <span>
+                <img src="#" alt="Clear" />
+              </span>
+              Temp °F
+            </h3>
+          </div>
+        </div>
+        <h4 className="mt-4">FORECAST</h4>
+      </div>
     </div>
   );
 }
