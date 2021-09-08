70b175441ff2043ff99a56b7c0d8dfa1ca80f889
catnguyen90 <catnguyen90@gmail.com>
2021-09-08 17:51:39 -0400

    Corrected the coordinates by using useEffect, removed the celsius temperature conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function celsius() {
-    return ((props.fahrenheit - 32) * 5) / 9;
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <h3 className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit">
-          °F |{" "}
-          <a href="/" onClick={showCelsius}>
-            °C{" "}
-          </a>
-        </span>
-      </h3>
-    );
-  } else {
-    return (
-      <h3 className="WeatherTemperature">
-        <span className="temperature">{Math.round(celsius())}</span>
-        <span className="unit">
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>{" "}
-          | °C{" "}
-        </span>
-      </h3>
-    );
-  }
+  return (
+    <h3 className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+      <span className="unit">°F</span>
+    </h3>
+  );
 }
